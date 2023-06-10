1b26153507a03a6b2208f864afd79cb0b176db6d
sourovch <sourovch@gmail.com>
2023-06-10 10:58:45 +0600

    fix data file path

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import HorizontalScroll from "@/components/horizontalScroll";
 import { promises as fs } from "fs";
+import path from "path";
 
 export default function Page({ data }) {
   return <HorizontalScroll imgArr={data} />;
 }
 
 export async function getServerSideProps() {
-  const res = await fs.readFile("./public/data.json", "utf8");
+  const filePath = path.join(process.cwd(), "data.json");
+  const res = await fs.readFile(filePath, "utf8");
 
   const data = JSON.parse(res);
 
